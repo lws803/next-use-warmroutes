63080f6a735ce749216561ec807bf920f96b3743
lws803 <lws803@gmail.com>
2023-05-11 20:11:13 -0400

    use focus event to warm routes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { minimatch } from "minimatch";
 
 export default function useWarmRoutes(routesMapping: Record<string, string[]>) {
   const pathname = usePathname();
 
-  useEffect(() => {
-    const warmupPaths = (paths: string[]) => {
-      paths.forEach((path) => {
-        fetch(path, { method: "OPTIONS" }).catch(() => null);
-      });
-    };
+  const wakeRoutes = useCallback(
+    (pathname: string) => {
+      const warmupPaths = (paths: string[]) => {
+        paths.forEach((path) => {
+          fetch(path, { method: "OPTIONS" }).catch(() => null);
+        });
+      };
 
-    for (const key in routesMapping) {
-      if (minimatch(pathname, key)) {
-        const pathsToWarmUp = routesMapping[key];
-        return warmupPaths(pathsToWarmUp);
+      for (const key in routesMapping) {
+        if (minimatch(pathname, key)) {
+          const pathsToWarmUp = routesMapping[key];
+          return warmupPaths(pathsToWarmUp);
+        }
       }
-    }
-  }, [pathname]);
+    },
+    [pathname]
+  );
+
+  useEffect(() => wakeRoutes(pathname), [pathname]);
+
+  useEffect(() => {
+    window.addEventListener("focus", () => wakeRoutes(pathname));
+    return () => {
+      window.removeEventListener("focus", () => wakeRoutes(pathname));
+    };
+  }, []);
 
   return {
     warmRoute: (paths: string[]) =>
